c231f2d0da10819f5a8444d50a78530ab9e83a82
Alexhb1233 <100882144+Alexhb1233@users.noreply.github.com>
2023-04-18 23:39:37 -0500

    Update Carnivore.cs

--- Ecology Simulation/Assets/Scripts/Organisms/Animals/Carnivores/Carnivore.cs
+++ Ecology Simulation/Assets/Scripts/Organisms/Animals/Carnivores/Carnivore.cs
@@ -1,4 +1,37 @@
 public class Carnivore : Animal
 {
-    
-}
+    private int hungerLevel;
+    private const int MAX_HUNGER_LEVEL = 10;
+    private int healthLevel;
+    private const int MAX_HEALTH_LEVEL = 10;
+
+    public Carnivore()
+    {
+        this.hungerLevel = MAX_HUNGER_LEVEL;
+        this.healthLevel = MAX_HEALTH_LEVEL;
+    }
+
+    public void Hunger(Fox fox)
+    {
+        if (this.hungerLevel < 6)
+        {
+          
+            fox.hunt(); 
+            this.hungerLevel++; 
+        }
+        
+         if (this.hungerLevel == 0)
+        {
+           
+            this.healthLevel--;
+        }
+        
+        else
+        {
+           return;
+        
+        }
+
+       
+    }
+
