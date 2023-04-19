d6120a9f0f424d3da89d02b7b65f7aa0f0f530aa
Alexhb1233 <100882144+Alexhb1233@users.noreply.github.com>
2023-04-18 23:43:37 -0500

    Update Herbivore.cs

--- Ecology Simulation/Assets/Scripts/Organisms/Animals/Herbivores/Herbivore.cs
+++ Ecology Simulation/Assets/Scripts/Organisms/Animals/Herbivores/Herbivore.cs
@@ -1,4 +1,33 @@
 public class Herbivore : Animal
 {
-    
+    private int hungerLevel;
+    private const int MAX_HUNGER_LEVEL = 10;
+    private int healthLevel;
+    private const int MAX_HEALTH_LEVEL = 10;
+
+    public Herbivore()
+    {
+        this.hungerLevel = MAX_HUNGER_LEVEL;
+        this.healthLevel = MAX_HEALTH_LEVEL;
+    }
+
+    public void Eat(Rabbit rabbit)
+    {
+        if (this.hungerLevel >= 6)
+        {
+            rabbit.Eat(); 
+            this.hungerLevel--; 
+        }
+        
+        while(this.hungerLevel == 0){
+        healthLevel--;
+        
+        }
+        else
+        {
+            return;
+        }
+
+        
+    }
 }
