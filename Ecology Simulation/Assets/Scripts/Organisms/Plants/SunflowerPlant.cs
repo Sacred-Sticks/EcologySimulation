7103e5aae2b0c8aaad7a28e1dfc1b8a3b3fb8f3e
Lucas Ackman <AckmanLD@gmail.com>
2023-05-03 11:26:08 -0500

    Update SunflowerPlant.cs

--- Ecology Simulation/Assets/Scripts/Organisms/Plants/SunflowerPlant.cs
+++ Ecology Simulation/Assets/Scripts/Organisms/Plants/SunflowerPlant.cs
@@ -1,52 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Essentials.References;
+
 
 public class SunflowerPlant : Plant
 {
-    public FloatReference hydrationRange2;
-    public FloatReference reproductionRange2;
-    public float hydrationTimer;
-    public float reproductionCooldown;
-    private float reproductionTimer;
-
-    protected override void Awake()
-    {
-        base.Awake();
-
-        hydrationTimer = hydrationRange2.Value;
-        reproductionTimer = 0f;
-    }
-
-    protected override void Update()
-    {
-        base.Update();
-
-        // Check hydration timer
-        if (hydrationTimer <= 0)
-        {
-            // Destroy the game object
-            Destroy(gameObject);
-        }
-        else
-        {
-            hydrationTimer -= Time.deltaTime;
-        }
-
-        // Check reproduction timer
-        if (reproductionTimer <= 0)
-        {
-            // Spawn a new game object nearby
-            Vector3 spawnPosition = transform.position + Random.insideUnitSphere * reproductionRange2.Value;
-            Instantiate(gameObject, spawnPosition, Quaternion.identity);
-
-            // Reset reproduction timer
-            reproductionTimer = reproductionCooldown;
-        }
-        else
-        {
-            reproductionTimer -= Time.deltaTime;
-        }
-    }
+    
 }
