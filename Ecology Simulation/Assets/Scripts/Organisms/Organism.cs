9b0e55da20273f79cb15ca5903cd1bae5fc463f7
LyndenJT17 <64235625+LyndenJT17@users.noreply.github.com>
2023-05-03 09:36:06 -0500

    cloneable object

--- Ecology Simulation/Assets/Scripts/Organisms/Organism.cs
+++ Ecology Simulation/Assets/Scripts/Organisms/Organism.cs
@@ -5,9 +5,6 @@ public class Organism : MonoBehaviour
 {
     [SerializeField] protected FloatReference sustenance;
     [SerializeField] protected FloatReference hydration;
-    [SerializeField] protected FloatReference reproductionCooldown;
-    [SerializeField] protected EnumeratedObject species;
-
     protected float Sustenance
     {
         get
@@ -19,6 +16,7 @@ public class Organism : MonoBehaviour
             sustenance.Value = value;
         }
     }
+
     protected float Hydration
     {
         get
@@ -30,17 +28,8 @@ public class Organism : MonoBehaviour
             hydration.Value = value;
         }
     }
-    protected float ReproductionCooldown
-    {
-        get
-        {
-            return reproductionCooldown.Value;
-        }
-        set
-        {
-            reproductionCooldown.Value = value;
-        }
-    }
+
+    protected float ReproductionCooldown { get; set; }
 
     public static Vector3 GetRandomDirection()
     {
@@ -48,5 +37,25 @@ public class Organism : MonoBehaviour
         float y = Random.Range(-1f, 1f);
         var direction = new Vector3(x, y, 0).normalized;
         return direction;
+    }
+
+    protected virtual void Awake()
+    {
+        Sustenance = Random.Range(20f, 40f);
+        Hydration = Random.Range(20f, 40f);
+        ReproductionCooldown = 0;
+    }
+
+    protected virtual void Update()
+    {
+        // Decrease sustenance and hydration over time
+        Sustenance -= Time.deltaTime;
+        Hydration -= Time.deltaTime;
+
+        // Check for death
+        if (Sustenance <= 0 || Hydration <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Ecology Simulation/Assets/Scripts/Organisms/Plants/Plant.cs
+++ Ecology Simulation/Assets/Scripts/Organisms/Plants/Plant.cs
@@ -5,14 +5,47 @@ public class Plant : Organism
 {
     [SerializeField] protected FloatReference hydrationRange;
     [SerializeField] protected FloatReference reproductionRange;
-    protected virtual void Awake()
-    {
-        // ...
-    }
-    protected virtual void Update()
-    {
-        // ...
-    }
     protected float HydrationRange { get; set; }
     protected float ReproductionRange { get; set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        HydrationRange = hydrationRange.Value;
+        ReproductionRange = reproductionRange.Value;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        // Check hydration
+        if (Hydration <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Hydration -= Time.deltaTime;
+        }
+
+        // Check reproduction cooldown
+        if (ReproductionCooldown > 0)
+        {
+            ReproductionCooldown -= Time.deltaTime;
+        }
+    }
+
+    protected virtual void Reproduce()
+    {
+        if (ReproductionCooldown <= 0)
+        {
+            // Spawn a new game object nearby
+            Vector3 spawnPosition = transform.position + Random.insideUnitSphere * ReproductionRange;
+            Instantiate(gameObject, spawnPosition, Quaternion.identity);
+
+            // Reset reproduction cooldown
+            ReproductionCooldown = Random.Range(10f, 20f);
+        }
+    }
 }
